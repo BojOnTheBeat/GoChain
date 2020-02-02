9b97fe8e20124a1bbb54d3f2f11ac709a5c5efec
BojOnTheBeat <bjfatade@gmail.com>
2020-02-01 22:45:25 -0500

    Implement a main function for testing

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	bc := NewBlockChain()
+
+	bc.AddBlock("Send 1 BTC to Jon")
+	bc.AddBlock("Send 2 more BTC to Jon")
+
+	for _, block := range bc.blocks {
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 
 }
